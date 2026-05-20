6b129f50d06f5306bd643b86138945abab886870
elya <vavina.elvira@gmail.com>
2026-05-21 01:53:56 +0300

    basic Printf

--- fmts.go
+++ fmts.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 func main() {
 	for i := range 3 {
@@ -26,4 +30,13 @@ func main() {
 	fmt.Printf("res: %+08.2f\n", 3.148) // res: +0003.15
 	fmt.Printf("|%5d|\n", 123)          // | 123 |
 	fmt.Printf("|%-5d|\n", 456)         // |456  |
+	var ttt int8 = -2
+	fmt.Println(uint8(ttt)) // 254 (overflow)
+	fmt.Println(string(65), strconv.Itoa(65))
+	fl := 3.7
+	fmt.Println(int(fl), int(math.Round(fl))) // 3 4 // int(3.7) error with no var
+	// fmt.Println(uint8(300))                   // constant 300 overflows uint8 (44 with var)
+	fmt.Println(math.MaxInt32, math.Pow(2, 31)-1 == math.MaxInt32)
+	valu := -2809
+	fmt.Println(uint8(valu))
 }
